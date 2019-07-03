701467e4278df56d2aae676e750868ae462fb073
tunglv <lvtungdn43@gmail.com>
2019-07-03 16:55:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Row } from 'reactstrap';
+import Product from './component/Product';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let products = [
+      {
+        id : 1,
+        images: "https://znews-photo.zadn.vn/w480/Uploaded/sotiuv/2019_06_28/4iphone.jpg",
+        title: "iPhone và phận ‘nuôi con tu hú’ của Trung Quốc",
+        text: "“Mổ” chiếc iPhone sẽ thấy giá trị mà Trung Quốc nhận được rất thấp so với những công ty tham gia vào quá trình sản xuất. Apple mới là bên hưởng lợi khủng khiếp.",
+        status: true,
+      },
+      {
+        id : 2,
+        images: "https://znews-photo.zadn.vn/w480/Uploaded/sotiuv/2019_06_28/1ive.jpg",
+        title: "Cảm ơn Jony Ive, Apple sẽ tốt hơn khi không có ông",
+        text: "Jony Ive đã trở thành biểu tượng cho những thiết kế sáng tạo của Apple. Chia tay một huyền thoại là cơ hội để táo khuyết viết nên trang sử mới.",
+        status: false,
+      },
+      {
+        id : 3,
+        images: "https://znews-photo.zadn.vn/w480/Uploaded/lce_jwquc/2019_06_24/tech2.jpg",
+        title: "Công nghệ Trung Quốc phụ thuộc Mỹ như thế nào",
+        text: "Trung Quốc phụ thuộc vào Mỹ trong những công nghệ quan trọng nhất như bán dẫn, hệ điều hành, chip... dù đã tự chủ trong các lĩnh vực liên quan đến tiêu dùng và mạng xã hội.",
+        status: false,
+      }
+    ];
+
+    let elements = products.map((product, index) => {
+      let result = '';
+      if(product.status === true) {
+        return result = <Product 
+                  key={index}
+                  images={product.images}
+                  title={product.title}
+                  text={product.text}
+                />
+      } else {
+        return result;
+      }
+    });
+    
+    return (
+      <React.Fragment>
+        <Container>
+          <Row>
+            {elements}
+          </Row>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Product.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Col, Card, CardImg, CardBody, CardText, CardTitle, Button } from 'reactstrap';
+
+class Product extends Component {
+    render() {
+
+        const styleTitle = {
+            fontSize: '30px',
+            fontWeight: 'bold'
+        }
+
+        return (
+            <Col xs="3">
+                <Card>
+                    <CardImg top width="100%" height="100%" src={this.props.images} alt={this.props.title} />
+                    <CardBody>
+                        <CardTitle style={styleTitle}>{this.props.title}</CardTitle>
+                        <CardText>{this.props.text}</CardText>
+                        <Button color="info">Button</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }
+}
+
+export default Product;
